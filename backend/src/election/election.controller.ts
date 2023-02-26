34bd8a5d1810be9ba34be628d1a57763d5b8dda8
Independence <nguyenkhanh17112003@gmail.com>
2023-02-27 00:38:57 +0700

    election restfull

--- backend/src/election/election.controller.ts
+++ backend/src/election/election.controller.ts
@@ -1,5 +1,63 @@
-import { Body, Controller } from '@nestjs/common';
-
+import {
+    Body,
+    Controller,
+    Get,
+    Param,
+    HttpCode,
+    HttpStatus,
+    Post,
+    Req,
+    UseGuards,
+    ParseUUIDPipe,
+    Patch,
+    Delete,
+} from '@nestjs/common';
 import { ElectionService } from './election.service';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
+import { CreateElectionDto, EditElectionDto } from './dto';
 
-export class ElectionController {}
+@UseGuards(JwtGuard)
+@Controller('elections')
+export class ElectionController {
+    constructor(private electionService: ElectionService) {}
+
+    @Get()
+    getElections(@GetUser('id') userId: string) {
+        return this.electionService.getElections(userId);
+    }
+
+    @Get(':id')
+    getElectionById(
+        @GetUser('id') userId: string,
+        @Param('id', ParseUUIDPipe) electionId: string,
+    ) {
+        return this.electionService.getElectionById(userId, electionId);
+    }
+
+    @Post()
+    createElection(
+        @GetUser('id') userId: string,
+        @Body() dto: CreateElectionDto,
+    ) {
+        return this.electionService.createElection(userId, dto);
+    }
+
+    @Patch(':id')
+    editElectionById(
+        @GetUser('id') userId: string,
+        @Param('id', ParseUUIDPipe) electionId: string,
+        @Body() dto: EditElectionDto,
+    ) {
+        return this.electionService.editElectionById(userId, electionId, dto);
+    }
+
+    @HttpCode(HttpStatus.NO_CONTENT)
+    @Delete(':id')
+    deleteElectionById(
+        @GetUser('id') userId: string,
+        @Param('id', ParseUUIDPipe) electionId: number,
+    ) {
+        return this.electionService.deleteElectionById(userId, electionId);
+    }
+}
