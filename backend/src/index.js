ab90b73671770b13ecff4822e41a3baa5372a369
Independence <nguyenkhanh17112003@gmail.com>
2023-03-10 10:42:46 +0700

    backend

---
+++ backend/src/index.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import router from "./routes/index.routes";
+
+
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+router(app);
+
+
+const PORT = process.env.PORT || 5000;
+const start = async function() {
+    try {
+        app.listen(PORT,  function() {
+            console.log(`http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+;(()=> {
+    start();
+})()
---
+++ backend/src/routes/index.routes.js
@@ -0,0 +1,7 @@
+
+
+const router = function() {
+
+}
+
+export default router;
